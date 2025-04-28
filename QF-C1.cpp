10e0296de82123081eaba79ed3f895d6e73e3f10
Marcus1Vinicius <omarcusvinicius96@gmail.com>
2025-04-28 04:41:03 -0500

    adicionando questão F da contest 1

---
+++ QF-C1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int contador;
+    cin >> contador;
+    string N;
+    for(int i = 0; i < contador; i++){
+        cin >> N; cout << N;
+    }
+
+}
